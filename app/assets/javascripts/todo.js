af3fd502ccf1872b49a36981c4b30cbe479cfcf0
Myles Byrne <myles@myles.id.au>
2013-04-26 11:33:17 -0700

    Remove the old UI implementation to make way for the new one

--- app/assets/javascripts/todo.js
+++
@@ -1,9 +0,0 @@
-var Todo = function(data) {
-  this.title = data.title;
-  this.body = data.body;
-  this.id   = data.id;
-}
-
-Todo.prototype.render = function() {
-  return HandlebarsTemplates['todo-item']({title: this.name, body: this.body, id: this.id})
-}
